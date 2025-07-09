c2aa4d00bbfc857fe41e7332e7f6cc8e24b337c3
abhirampasaladi <abhiraampasaladi07@gmail.com>
2025-07-09 17:59:24 -0400

    backend push

---
+++ ew_backend/src/main/java/org/project/ew_backend/EwBackendApplication.java
@@ -0,0 +1,13 @@
+package org.project.ew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwBackendApplication.class, args);
+    }
+
+}
