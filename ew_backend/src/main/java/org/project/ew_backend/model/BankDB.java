e324ee58aff43d5d3b4c726fa3316b8cf830379b
abhirampasaladi <abhiraampasaladi07@gmail.com>
2025-07-10 08:45:45 -0400

    working on code

--- ew_backend/src/main/java/org/project/ew_backend/model/BankDB.java
+++ ew_backend/src/main/java/org/project/ew_backend/model/BankDB.java
@@ -1,17 +1,17 @@
-package org.project.ew_backend.model;
-
-public class BankDB {
-    private String id;
-    private String username;
-    private String password;
-    private String email;
-    private String phone;
-
-    BankDB(String id, String username, String password, String email, String phone) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.phone = phone;
-    }
-}
+//package org.project.ew_backend.model;
+//
+//public class BankDB {
+//    private String id;
+//    private String username;
+//    private String password;
+//    private String email;
+//    private String phone;
+//
+//    BankDB(String id, String username, String password, String email, String phone) {
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//        this.phone = phone;
+//    }
+//}
